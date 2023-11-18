73fd524f553ec680ff74362e10410e868910c97a
JSheare <jrsheare@ucsc.edu>
2023-11-18 14:07:11 -0800

    Voice easter egg bugfix

--- greepbot.py
+++ greepbot.py
@@ -71,7 +71,8 @@ class Greepbot(discord.Client):
                     break
 
             # Rolls dice on voice channel easter egg
-            await self.roll_dice(message)
+            if 'greepbot' in message.content:
+                await self.roll_dice(message)
 
     # Sends a random Greep quote from the list
     async def send_quote(self, message):
